d3ac5a8b8b3632dd55a8735083edb2ad8bfdcf92
julietcohen <jscohen@bren.ucsb.edu>
2021-10-18 11:33:59 -0700

    added west buoy data and added file name assingment to r script

--- wind_data.R
+++ wind_data.R
@@ -3,3 +3,5 @@ library(here)
 noaa_east_buoy_data <- read.csv(here("data", "noaa_wind_data.csv"))
 
 noaa_santa_monica_buoy_data <- read.csv(here("data", "noaa_santa_monica_data.csv"))
+
+noaa_west_buoy_data <- read.csv(here("data", "noaa_west_data.csv"))
